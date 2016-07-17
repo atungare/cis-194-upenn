2e267d8af7af280249fa11591ce20c3df74902b2
atungare <ajay.tungare@gmail.com>
2016-07-17 15:18:18 -0400

    working ch3

--- ch3/golf.hs
+++ ch3/golf.hs
@@ -9,11 +9,21 @@ every n xs = case drop (n-1) xs of
   [] -> []
 
 skips :: [a] -> [[a]]
-skips xs = map (\n -> every n xs) [1..(length xs)]
+skips xs = map (flip every xs) [1..(length xs)]
 
 -- E2
 localMaxima :: (Ord a) => [a] -> [a]
 localMaxima xs = [y | (x:y:z:_) <- tails xs, y > x && y > z]
 
 -- E3
---histogram :: [Integer] -> String
+count :: (Eq a) => a -> [a] -> Int
+count x = length . (filter (==x))
+
+histogram :: [Integer] -> String
+histogram xs = unlines ([ buildRow n | n <- height] ++ [['=' | m <- range]] ++ [concat [show m | m <- range]])
+  where
+    range = [0..9]
+    nums = map (flip count xs) range
+    mx = maximum nums
+    height = [mx, mx-1..1]
+    buildRow n = map (\x -> if x >= n then '*' else ' ') nums
